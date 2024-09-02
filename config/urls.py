ef3d9243ae22dc878f0f5ce9fabbe2ed118938db
inno-waluza <innowaluza@gmail.com>
2024-09-02 09:47:20 +0200

    url update

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('nutrition_analyzer.urls')),
+    path('', include('nutrition_analyzer.urls')),  # Remove the 'api/' prefix
 ]
-
