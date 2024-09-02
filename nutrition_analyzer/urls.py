f3f1688f531976b7dd8ecf8f17d806de76600f0c
inno-waluza <innowaluza@gmail.com>
2024-09-02 15:46:13 +0200

    urls changes2

--- nutrition_analyzer/urls.py
+++ nutrition_analyzer/urls.py
@@ -3,5 +3,5 @@ from .views import NutritionAnalyzerView, AnalyzeInterface
 
 urlpatterns = [
     path('analyze/', NutritionAnalyzerView.as_view(), name='analyze'),
-    path('analyzer/', AnalyzeInterface.as_view(), name='analyze_interface'),
+    path('', AnalyzeInterface.as_view(), name='analyze_interface'),
 ]
